075602bbf786c6f2fbc37d3c952539a66e3cc835
kylehovey <khoveyc@gmail.com>
2019-12-05 11:14:37 -0700

    Switched timeout to formal parameter

--- src/action.js
+++ src/action.js
@@ -5,20 +5,14 @@
 //   body      - The body of the request. Used for POST and PUT
 //   localData - An object containing data that the action will hold onto for local usage
 //               but will not be passed to the server.
-export default function(canvas, params, body, localData = {}) {
+//   timeout   - Override the default network timeout for this request
+export default function(canvas, params, body, localData = {}, timeout) {
   return {
     type: canvas.type,
     canvas,
     params,
     body,
     localData,
-
-    /**
-     * Usage of this utility method:
-     * canvasRequest(canvas, params, body).withTimeout(60e3);
-     */
-    withTimeout(timeout) {
-      return { ...this, timeout };
-    },
+    timeout,
   };
 }
