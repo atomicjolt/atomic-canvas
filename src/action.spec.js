2243cf3d8ad385c3bd0c4636403d268968f47be1
kylehovey <khoveyc@gmail.com>
2019-12-05 11:23:27 -0700

    Added spec

--- src/action.spec.js
+++ src/action.spec.js
@@ -1,7 +1,6 @@
 import canvasRequest         from './action';
 
 describe('canvasRequest', () => {
-
   it('Generates an action that can be dispatched to canvas middleware', () => {
     const listAccounts = { type: 'LIST_ACCOUNTS', method: 'get', reducer: 'accounts' };
     const action = canvasRequest(listAccounts, { course_id: 1 });
@@ -10,4 +9,15 @@ describe('canvasRequest', () => {
     expect(action.canvas.type).toBe('LIST_ACCOUNTS');
   });
 
+  it('Supports adding localData to request', () => {
+    const listAccounts = { type: 'LIST_ACCOUNTS', method: 'get', reducer: 'accounts' };
+    const action = canvasRequest(listAccounts, { course_id: 1 }, null, { thing: true });
+    expect(action.localData.thing).toBeTruthy();
+  });
+
+  it('Supports network timeout', () => {
+    const listAccounts = { type: 'LIST_ACCOUNTS', method: 'get', reducer: 'accounts' };
+    const action = canvasRequest(listAccounts, { course_id: 1 }, null, null, 60e3);
+    expect(action.timeout).toBe(60e3);
+  });
 });
